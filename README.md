bbf660c6fb9a92e17e4697c9a3b5a69906355e7b
Juan Ignacio Caprioli <juanicaprioli16@outlook.com>
2024-01-09 15:34:31 -0300

    Mi primer commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Proyecto Web We Make Noise
+
+## Descripción
+Este proyecto web es parte del curso CODERHOUSE Desarrollo Web Flex donde el programa se basa en un cliente que necesita un sitio web de marketing, donde se debe atraer la atención al cliente que necesiten de marketing y puedan contactarse a una casilla de correo o por llamada. El objetivo principal es diseñar e implementar un sitio web para We Make Noise con fines educativos y de práctica profesional.
+
+## Equipo de Desarrollo
+- Juan Ignacio Caprioli https://github.com/CHANOCHOCA
+
+## Tecnologías Utilizadas
+- Lenguajes de Programación: Java, HTML, CSS, JavaScript
+- Framework: Spring Boot
+- Base de Datos: PosgreSQL
+- Herramientas de Desarrollo: Intellij IDEA
+- Control de Versiones: Git
+- Planificación: Whimsical
+
+## Funcionalidades
+- [ ] Página de inicio (enfocado a atraer la atención al cliente).
+- [ ] Blog con posts.
+- [ ] Información de la empresa.
+- [ ] Formulario de contacto.
+- [ ] Enlace al número telefónico.
---
+++ js/script.js
@@ -0,0 +1,52 @@
+//navbar
+let prevScrollpos = window.scrollY;
+
+window.onscroll = function () {
+    const currentScrollPos = window.scrollY;
+    const navbarButton = document.querySelector(".navbar-toggler");
+
+    if (currentScrollPos > prevScrollpos) {
+        document.querySelector(".navbar").style.top = "-101px";
+        if (navbarButton.getAttribute("aria-expanded") === "true") {
+            document.querySelector(".navbar").style.top = "0";
+        }
+    } else {
+        document.querySelector(".navbar").style.top = "0";
+    }
+
+    prevScrollpos = currentScrollPos;
+};
+
+//testimonios
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".boton-acordeon");
+
+    buttons.forEach(function (button, index) {
+        button.addEventListener("click", function () {
+            toggleImage(index);
+        });
+    });
+
+    function toggleImage(index) {
+        buttons.forEach(function (btn, i) {
+            const img = btn.querySelector("img");
+            img.src = i === index ? toggleSrc(img.src) : "assets/index/boton.svg";
+        });
+    }
+
+    function toggleSrc(src) {
+        return src.endsWith("boton-relleno.svg") ? "assets/index/boton.svg" : "assets/index/boton-relleno.svg";
+    }
+});
+
+window.addEventListener('scroll', function() {
+    const elements = document.getElementsByClassName('scroll-animate');
+    Array.from(elements).forEach(function(element) {
+        const position = element.getBoundingClientRect();
+
+        // checking whether fully visible
+        if(position.top >= 0 && position.bottom <= window.innerHeight) {
+            element.classList.add('show');
+        }
+    });
+});
